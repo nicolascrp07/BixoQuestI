01aadfc7aa32130b8b5e946ec3c3410ee804caa9
Nícolas Carvalho <nicolascrp07@gmail.com>
2026-04-04 18:27:34 -0300

    Inicio do desenvolvimento de PartidaService.

--- src/main/java/model/service/PartidaService.java
+++ src/main/java/model/service/PartidaService.java
@@ -1,4 +1,26 @@
 package main.java.model.service;
 
+import main.java.model.entity.academic.Disciplina;
+import main.java.model.entity.character.Jogador;
+import main.java.model.entity.game.Tempo;
+import main.java.model.entity.world.Universidade;
+
+import java.util.ArrayList;
+
 public class PartidaService {
+    public void avancarSemana(Jogador j, Universidade uni, Tempo tempo, AcademicoService ac, ExplorarService ex, ArrayList<Disciplina> catalogo){
+       // preciso identificar a lógica com o ponto de ônibus
+    }
+
+    public boolean verificarFimDeJogo(Jogador j, int totalDisciplinasDoCurso){
+        return j.getHistoricoAprovadas().size() == totalDisciplinasDoCurso;
+    }
+
+    public void iniciarJogo(Universidade uni, Jogador j, Tempo t){
+        // ainda vou definir o preenchimento padrão! só os detalhes mesmo
+    }
+
+    public boolean verificarGameOver(Jogador j){
+        return j.getSaude() == 0 || j.getMotivacao() == 0;
+    }
 }
